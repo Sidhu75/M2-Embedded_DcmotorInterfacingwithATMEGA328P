3ca0ed8e9195276eda569bbeb49fb645d6f710e9
Sidhu75 <94374211+Sidhu75@users.noreply.github.com>
2021-12-01 21:39:39 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,1 +1,29 @@
 
+/*
+ * dcmotor1.c
+ *
+ * Created: 01-12-2021 15:30:17
+ * Author : ROHIT
+ */ 
+
+
+#include <avr/io.h>
+int main(void)
+{
+  DDRB=0x00;
+  PORTB=0xFF;
+  DDRC=0xFF;
+  while(1)
+  {
+if(PINB&0b00000001)//02
+{
+PORTC=0x00;   
+}
+else
+{ 
+PORTC=0x01; 
+}
+  }
+  return 0;
+}
+
